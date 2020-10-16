5e76b951cdbdd6a754ad6a608ca98e998c8e15c6
미니 변 <oaksnlo@naver.com>
2020-10-16 17:42:27 +0900

    초기 커밋.

---
+++ WinCalculator/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace WinCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btn7 = new System.Windows.Forms.Button();
+            this.Btn8 = new System.Windows.Forms.Button();
+            this.Btn9 = new System.Windows.Forms.Button();
+            this.Btn4 = new System.Windows.Forms.Button();
+            this.Btn5 = new System.Windows.Forms.Button();
+            this.Btn6 = new System.Windows.Forms.Button();
+            this.Btn1 = new System.Windows.Forms.Button();
+            this.Btn2 = new System.Windows.Forms.Button();
+            this.Btn3 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.lblCal = new System.Windows.Forms.Label();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Btn7
+            // 
+            this.Btn7.Font = new System.Drawing.Font("나눔고딕 ExtraBold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Btn7.Location = new System.Drawing.Point(12, 139);
+            this.Btn7.Name = "Btn7";
+            this.Btn7.Size = new System.Drawing.Size(65, 50);
+            this.Btn7.TabIndex = 0;
+            this.Btn7.Text = "7";
+            this.Btn7.UseVisualStyleBackColor = true;
+            this.Btn7.Click += new System.EventHandler(this.Btn_Click);
+            // 
+            // Btn8
+            // 
+            this.Btn8.Font = new System.Drawing.Font("나눔고딕 ExtraBold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Btn8.Location = new System.Drawing.Point(90, 139);
+            this.Btn8.Name = "Btn8";
+            this.Btn8.Size = new System.Drawing.Size(65, 50);
+            this.Btn8.TabIndex = 1;
+            this.Btn8.Text = "8";
+            this.Btn8.UseVisualStyleBackColor = true;
+            this.Btn8.Click += new System.EventHandler(this.Btn_Click);
+            // 
+            // Btn9
+            // 
+            this.Btn9.Font = new System.Drawing.Font("나눔고딕 ExtraBold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Btn9.Location = new System.Drawing.Point(168, 139);
+            this.Btn9.Name = "Btn9";
+            this.Btn9.Size = new System.Drawing.Size(65, 50);
+            this.Btn9.TabIndex = 2;
+            this.Btn9.Text = "9";
+            this.Btn9.UseVisualStyleBackColor = true;
+            this.Btn9.Click += new System.EventHandler(this.Btn_Click);
+            // 
+            // Btn4
+            // 
+            this.Btn4.Font = new System.Drawing.Font("나눔고딕 ExtraBold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Btn4.Location = new System.Drawing.Point(12, 200);
+            this.Btn4.Name = "Btn4";
+            this.Btn4.Size = new System.Drawing.Size(65, 50);
+            this.Btn4.TabIndex = 3;
+            this.Btn4.Text = "4";
+            this.Btn4.UseVisualStyleBackColor = true;
+            this.Btn4.Click += new System.EventHandler(this.Btn_Click);
+            // 
+            // Btn5
+            // 
+            this.Btn5.Font = new System.Drawing.Font("나눔고딕 ExtraBold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Btn5.Location = new System.Drawing.Point(90, 200);
+            this.Btn5.Name = "Btn5";
+            this.Btn5.Size = new System.Drawing.Size(65, 50);
+            this.Btn5.TabIndex = 4;
+            this.Btn5.Text = "5";
+            this.Btn5.UseVisualStyleBackColor = true;
+            this.Btn5.Click += new System.EventHandler(this.Btn_Click);
+            // 
+            // Btn6
+            // 
+            this.Btn6.Font = new System.Drawing.Font("나눔고딕 ExtraBold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Btn6.Location = new System.Drawing.Point(168, 200);
+            this.Btn6.Name = "Btn6";
+            this.Btn6.Size = new System.Drawing.Size(65, 50);
+            this.Btn6.TabIndex = 5;
+            this.Btn6.Text = "6";
+            this.Btn6.UseVisualStyleBackColor = true;
+            this.Btn6.Click += new System.EventHandler(this.Btn_Click);
+            // 
+            // Btn1
+            // 
+            this.Btn1.Font = new System.Drawing.Font("나눔고딕 ExtraBold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Btn1.Location = new System.Drawing.Point(12, 261);
+            this.Btn1.Name = "Btn1";
+            this.Btn1.Size = new System.Drawing.Size(65, 50);
+            this.Btn1.TabIndex = 6;
+            this.Btn1.Text = "1";
+            this.Btn1.UseVisualStyleBackColor = true;
+            this.Btn1.Click += new System.EventHandler(this.Btn_Click);
+            // 
+            // Btn2
+            // 
+            this.Btn2.Font = new System.Drawing.Font("나눔고딕 ExtraBold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Btn2.Location = new System.Drawing.Point(90, 261);
+            this.Btn2.Name = "Btn2";
+            this.Btn2.Size = new System.Drawing.Size(65, 50);
+            this.Btn2.TabIndex = 7;
+            this.Btn2.Text = "2";
+            this.Btn2.UseVisualStyleBackColor = true;
+            this.Btn2.Click += new System.EventHandler(this.Btn_Click);
+            // 
+            // Btn3
+            // 
+            this.Btn3.Font = new System.Drawing.Font("나눔고딕 ExtraBold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Btn3.Location = new System.Drawing.Point(168, 261);
+            this.Btn3.Name = "Btn3";
+            this.Btn3.Size = new System.Drawing.Size(65, 50);
+            this.Btn3.TabIndex = 8;
+            this.Btn3.Text = "3";
+            this.Btn3.UseVisualStyleBackColor = true;
+            this.Btn3.Click += new System.EventHandler(this.Btn_Click);
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("나눔고딕 ExtraBold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button10.Location = new System.Drawing.Point(90, 322);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(65, 50);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "0";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.Btn_Click);
+            // 
+            // lblCal
+            // 
+            this.lblCal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCal.Font = new System.Drawing.Font("나눔고딕 ExtraBold", 21F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblCal.Location = new System.Drawing.Point(9, 21);
+            this.lblCal.Name = "lblCal";
+            this.lblCal.Size = new System.Drawing.Size(307, 71);
+            this.lblCal.TabIndex = 10;
+            this.lblCal.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // button11
+            // 
+            this.button11.Font = new System.Drawing.Font("나눔고딕 ExtraBold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button11.Location = new System.Drawing.Point(12, 322);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(65, 50);
+            this.button11.TabIndex = 11;
+            this.button11.Text = "CE";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.Btn_Click);
+            // 
+            // button12
+            // 
+            this.button12.Font = new System.Drawing.Font("나눔고딕 ExtraBold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button12.Location = new System.Drawing.Point(168, 322);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(65, 50);
+            this.button12.TabIndex = 12;
+            this.button12.Text = "=";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.Btn_Click);
+            // 
+            // button13
+            // 
+            this.button13.Font = new System.Drawing.Font("나눔고딕 ExtraBold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button13.Location = new System.Drawing.Point(246, 139);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(65, 50);
+            this.button13.TabIndex = 13;
+            this.button13.Text = "%";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.Btn_Click);
+            // 
+            // button14
+            // 
+            this.button14.Font = new System.Drawing.Font("나눔고딕 ExtraBold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button14.Location = new System.Drawing.Point(246, 200);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(65, 50);
+            this.button14.TabIndex = 14;
+            this.button14.Text = "*";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.Btn_Click);
+            // 
+            // button15
+            // 
+            this.button15.Font = new System.Drawing.Font("나눔고딕 ExtraBold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button15.Location = new System.Drawing.Point(246, 261);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(65, 50);
+            this.button15.TabIndex = 15;
+            this.button15.Text = "-";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.Btn_Click);
+            // 
+            // button16
+            // 
+            this.button16.Font = new System.Drawing.Font("나눔고딕 ExtraBold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button16.Location = new System.Drawing.Point(246, 322);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(65, 50);
+            this.button16.TabIndex = 16;
+            this.button16.Text = "+";
+            this.button16.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.Btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 394);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.lblCal);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.Btn3);
+            this.Controls.Add(this.Btn2);
+            this.Controls.Add(this.Btn1);
+            this.Controls.Add(this.Btn6);
+            this.Controls.Add(this.Btn5);
+            this.Controls.Add(this.Btn4);
+            this.Controls.Add(this.Btn9);
+            this.Controls.Add(this.Btn8);
+            this.Controls.Add(this.Btn7);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "계산기";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn7;
+        private System.Windows.Forms.Button Btn8;
+        private System.Windows.Forms.Button Btn9;
+        private System.Windows.Forms.Button Btn4;
+        private System.Windows.Forms.Button Btn5;
+        private System.Windows.Forms.Button Btn6;
+        private System.Windows.Forms.Button Btn1;
+        private System.Windows.Forms.Button Btn2;
+        private System.Windows.Forms.Button Btn3;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Label lblCal;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+    }
+}
+
